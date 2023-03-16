dbc1eed250a7c42b3750e5b7c8a45322d559d2a4
Cotti <35464832+Cottichan@users.noreply.github.com>
2023-03-16 11:49:31 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,59 @@
+// selección de zona para determinar si se puede hacer delivery
 
-// usuario debe seleccionar zona en que vive para determinar si está dentro de la zona de delivery
+function elegirDelegacion() {
+    const delOpciones = [
+      "Álvaro Obregón",
+      "Azcapotzalco",
+      "Benito Juárez",
+      "Coyoacán",
+    ];
+  
+    const delegacion = parseInt(prompt("Selecciona la delegación en la que vives:\n\n" + delOpciones.map((option, index) => `${index + 1}. ${option}`).join("\n")));
+  
+    return delegacion;
+  }
+  
+  function deliveryNo() {
+    alert("Lo sentimos. No tenemos envíos disponibles en tu zona.");
+  }
+  
+  function visitanos() {
+    alert("Puedes visitarnos en: Insurgentes Sur 601");
+  }
+  
+// selección de producto para determinar stock (para hacerlo corto para el trabajo, todos los productos están sin stock)
 
-let selection = prompt("Selecciona la delegación en la que vives (ingresa el número de la opción): \
- 1. Álvaro Obregón\
- 2. Azcapotzalco\
- 3. Benito Juárez\
- 4. Coyoacán");
-console.log(selection)
-let message = "";
-let address = "Insurgentes Sur 601";
-
-// si está dentro de la zona, se le pide elegir el producto que busca
-
-if (selection == 1 || selection == 4) {
-  let productSelection = prompt("Qué producto buscas? \
-  1. Café de especialidad\
-  2. Cafeteras\
-  3. Accesorios Café");
-  message = "fuera de stock";
-} else if (selection == 2 || selection == 3) {
-  message = "Lo sentimos. No tenemos envíos disponibles en tu zona.";
-} 
-
-alert(message + "\nPuedes visitarnos en: " + address);
+  function elegirProducto() {
+    const productoOpciones = [
+      "Café de especialidad",
+      "Cafeteras",
+      "Accesorios Café"
+    ];
+  
+    const productos = parseInt(prompt("Qué producto buscas?\n\n" + productoOpciones.map((option, index) => `${index + 1}. ${option}`).join("\n")));
+  
+    return productos;
+  }
+  
+  function noStock() {
+    alert("Fuera de stock.");
+  }
+  
+  function elegirDelegacion2() {
+    const delegacion = elegirDelegacion();
+  
+    if ([1, 4].includes(delegacion)) {
+      deliveryNo();
+    } else {
+      const elegirPro = elegirProducto();
+  
+      if ([1, 2, 3].includes(elegirPro)) {
+        noStock();
+      }
+    }
+  
+    visitanos();
+  }
+  
+  elegirDelegacion2();
+  
